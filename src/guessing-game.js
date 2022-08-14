5a46cdfc2aeb991b3525ad694015dfdb470670f4
potatosim <devildriver2310@gmail.com>
2022-08-15 01:23:54 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        return Math.ceil((this.end + this.start) / 2);
     }
 
     lower() {
-
+        this.end = this.guess();
     }
 
     greater() {
-
+        this.start = this.guess();
     }
 }
 
